b4f1b06e7a5bc5ff77798529b21ec5f371e2429c
Alvintz <alvintz.v@gmail.com>
2017-12-27 15:00:24 +0700

    add middleware

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/google/gops/agent"
 	"log"
 	"net/http"
@@ -10,6 +11,41 @@ import (
 	"gopkg.in/tokopedia/grace.v1"
 	"gopkg.in/tokopedia/logging.v1"
 )
+
+
+func logEndpoint(h http.HandlerFunc) http.HandlerFunc {
+
+	var count = 0
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var callx string
+    	count++
+
+    	if count == 11 || count == 12 || count == 13 {
+    		callx = fmt.Sprintf("%dth", count)
+    	} else if count % 10 == 1 {
+    		callx = fmt.Sprintf("%dst", count)
+    	} else if count % 10 == 2 {
+    		callx = fmt.Sprintf("%dnd", count)
+    	} else if count % 10 == 3 {
+    		callx = fmt.Sprintf("%drd", count)
+    	} else {
+    		callx = fmt.Sprintf("%dth", count)
+    	}
+    	log.Printf("This is %s call of this endpoint", callx)
+
+    	h.ServeHTTP(w, r)
+  })
+}
+
+func haii(h http.HandlerFunc) http.HandlerFunc {
+	
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Haiii~~")
+		
+		h.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 
@@ -28,7 +64,7 @@ func main() {
 
 	hwm := hello.NewHelloWorldModule()
 
-	http.HandleFunc("/hello", hwm.SayHelloWorld)
+	http.HandleFunc("/hello", haii(logEndpoint(hwm.SayHelloWorld)))
 	go logging.StatsLog()
 
 	log.Fatal(grace.Serve(":9000", nil))
