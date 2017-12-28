493677c3d955a62be5c87511e73a000c3b4e47e6
Vincent <vincentcent41@gmail.com>
2017-12-29 00:10:13 +0700

    Need to be tested

--- website/init.go
+++ website/init.go
@@ -7,30 +7,22 @@ import (
 	"text/template"
 	logging "gopkg.in/tokopedia/logging.v1"
 	"database/sql"
-    "fmt"
     _ "github.com/lib/pq"
+    "fmt"
     "github.com/jmoiron/sqlx"
     "github.com/garyburd/redigo/redis"
+    "time"
 )
 
 type User struct {
 	ID int `db:"user_id"`
-	Name string `db:"user_name"`
-	MSISDN string
-	Email string
-	birth_date time.Time
-	created_time time.Time
-	update_time time.Time
-	user_age int `db:"-"`
-}
-
-func init() {
-	*db, err = sqlx.Connect("postgres", "postgres://da161205:123Toped456@devel-postgre.tkpd/tokopedia-user?sslmode=disable")
-	if err != nil {
-        log.Fatalln(err)
-    }
-    *c, err = redis.Dial("tcp", "devel-redis.tkpd:6379")
-    c.Do("SET", "visitors", 0)
+	Name sql.NullString `db:"user_name"`
+	MSISDN sql.NullString
+	Email string `db:"user_email"`
+	BirthDate pq.NullTime `db:"birth_date"`
+	CreatedTime time.Time `db:"create_time"`
+	UpdateTime pq.NullTime `db:"update_time"`
+	UserAge int `db:"-"`
 }
 
 type ServerConfig struct {
@@ -43,11 +35,9 @@ type Config struct {
 
 type WebsiteModule struct {
 	cfg       *Config
-	visitorCount string
-	stats     *expvar.Int
 	render    *template.Template  //FOR TRAINING
-	db 		  *DB
-	c 		  Conn
+	db 		  *sqlx.DB
+	pool 	  *redis.Pool
 }
 
 func NewWebsiteModule() *WebsiteModule {
@@ -63,29 +53,62 @@ func NewWebsiteModule() *WebsiteModule {
 	// this message only shows up if app is run with -debug option, so its great for debugging
 	logging.Debug.Println("hello init called", cfg.Server.Name)
 
+	db, err := sqlx.Connect("postgres", "postgres://da161205:123Toped456@devel-postgre.tkpd/tokopedia-user?sslmode=disable")
+	if err != nil {
+        log.Fatalln(err)
+    }
+
 	//FOR TRAINING
 	engine := template.Must(template.ParseGlob("files/var/templates/*"))
 
 	return &WebsiteModule{
-		cfg:       &cfg,
-		visitorCount: c.Do("GET", "visitors"),
-		render:    engine,
+		cfg:       	&cfg,
+		render:    	engine,
+		db:			&db,
+		pool: 		&redis.Pool{
+			            MaxIdle:     3,
+			            IdleTimeout: 240 * time.Second,
+			            Dial: func() (redis.Conn, error) {
+			                conn, err := redis.Dial("tcp", "devel-redis.tkpd:6379")
+			                if err != nil {
+			                    return nil, err
+			                }
+			                return conn, err
+			          	},
+			        }
 	}
 }
 
 func (nwm *NewWebsiteModule) RenderWebpage(w http.ResponseWriter, r *http.Request) {
-	visitorCount := c.Do("INCR", "visitors")
+	conn := nwm.pool.Get()
+	visitorCount, err := conn.Do("INCR", "visitors")
+	if err != nil {
+		log.Println(err.Error())
+	}
 
 	user := []User{}
-	err1 := db.Select(&user, fmt.Sprintf("SELECT user_id, user_name, msisdn, email, birth_date, created_time, update_time, COALESCE(EXTRACT(YEAR from AGE(birth_date)),"0") AS user_age FROM WS_USER WHERE user_name LIKE '%%$1%%' ORDER BY user_name ASC LIMIT 10", r.formValue()))
+	user_name := "%"+r.FormValue("q")+"%"
+	var query string
+	if user_name != "%%" {
+		query = "SELECT user_id, COALESCE(user_name,'-'), COALESCE(msisdn,'-'), email, COALESCE(brith_date,'-'), COALESCE(create_time, date_trunc('second', now()::timestamp)), COALESCE(update_time, '-'), COALESCE(EXTRACT(YEAR from AGE(birth_date)),'0') AS user_age FROM WS_USER")
+	} else {
+		query = "SELECT user_id, COALESCE(user_name,'-'), COALESCE(msisdn,'-'), email, COALESCE(birth_date,'-'), COALESCE(create_time, date_trunc('second', now()::timestamp)), COALESCE(update_time, '-'), COALESCE(EXTRACT(YEAR from AGE(birth_date)),'0') AS user_age FROM WS_USER WHERE user_name LIKE $1 ORDER BY user_name ASC LIMIT 10;", user_name)
+	}
+	nwm.db.Select(&user, query)
 	
+	calculation := []String{}
+	for _, usr := range user {
+		calculation = append(fmt.Sprintf("%.1f", (usr.ID * 25.25)))
+	}
+
 	data := map[string]interface{}{
 		"user": user,
 		"visitorCount": visitorCount,
+		"calculation": calculation,
 	}
 
-	err2 := nwm.render.ExecuteTemplate(w, "home.html", data)
-	if err2 != nil {
+	err = nwm.render.ExecuteTemplate(w, "home.html", data)
+	if err != nil {
 		log.Println("Gagal Render Template because: ", err.Error())
 	}
 }
